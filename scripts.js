4f2a5773dac0f4cc19d40fabfe03dcf1898aa4f1
Tamarica Shaw <tamaricashaw13@gmail.com>
2023-10-30 22:33:31 -0500

    Added learn about layout and the hero overview section

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-/* Add your JavaScript to this file */
+
+ document.addEventListener("DOMContentLoaded", function () {
+    // Select the .overview-hero section
+    const overviewHeroSection = document.querySelector(".overview-hero");
+    const container = overviewHeroSection.querySelector(".container");
+    const img = container.querySelector(".overview-hero-image img"); // Select the <img> element directly
+
+    // Now, change the src attribute
+    img.src = "healthy_life.jpg";
+
+    const learnabout= document.querySelector(".learn-about");
+    const contain= learnabout.querySelector(".container");
+    const caRds = contain.querySelectorAll(".cards img");
+    
+    caRds[0].src = "ai_health.jpeg";
+    caRds[1].src = "fit_track.jpg";
+    caRds[2].src = "health.avif";
+
+});
+
+
